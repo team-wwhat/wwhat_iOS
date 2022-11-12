855229929472c4b17175659dc45ac4209d974d3a
BJCHO0501 <bjcho1503@naver.com>
2022-11-12 17:31:30 +0900

    📑 :: UIFont+Fonts

---
+++ Wwhat/Wwhat/Sources/Extention/UIFont+Fonts.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+extension UIFont {
+    static var title = UIFont(name: "NotoSansKR-Bold", size: 30)
+    static var subText = UIFont(name: "NotoSansKR-Light", size: 16)
+    static var buttonText = UIFont(name: "NotoSansKR-Medium", size: 13)
+    static var normalText = UIFont(name: "NotoSansKR-Medium", size: 16)
+    
+}
