b4ff735ff78853a3978e6170228a8ef859636457
BJCHO0501 <bjcho1503@naver.com>
2022-11-12 17:31:39 +0900

    📑 :: UIColor+Colors

---
+++ Wwhat/Wwhat/Sources/Extention/UIColor+Colors.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    // MARK: - main Color
+
+    static var main = UIColor(named: "E44545")
+    
+    // MARK: - 회색 베이스 Color
+
+    static var subText = UIColor(named: "747474")
+    static var tableBackground = UIColor(named: "F9F9F9")
+}
